a4b1a12977f3aab33a56bb88eb188bc5f586386d
egorummy <adam.mokrzycki22@gmail.com>
2025-06-18 19:20:13 +0200

    + task2

--- Project/src/App/DeveloperApp.java
+++ Project/src/App/DeveloperApp.java
@@ -4,7 +4,9 @@ import Product.Manager.ProductManager;
 import Product.Product;
 import Product.Storage;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public class DeveloperApp {
     public static void main(String[] args) {
@@ -14,6 +16,26 @@ public class DeveloperApp {
         products.forEach(productManager::addProduct);
 
         System.out.println("Lista dostępnych produktów");
+        productManager.getAllProducts().forEach(System.out::println);
+
+        Optional<Product> product1 = productManager.getProductById(products.get(2).getId());
+        product1.ifPresent(product -> System.out.println("Znaleziony produkt to: " + product.getName()));
+
+        Product smartphone = products.get(1);
+        Product updatedSmartphone = new Product(
+                smartphone.getId(),
+                smartphone.getName() + " model 2",
+                smartphone.getPrice().add(new BigDecimal("500")),
+                5,
+                smartphone.getProductType(),
+                smartphone.getConfigurableFeatures()
+        );
+        productManager.updateProduct(updatedSmartphone);
+
+        Product product2 = products.get(0);
+        productManager.removeProduct(product2.getId());
+
+        System.out.println("=== LISTA PRODUKTÓW ===");
         productManager.getAllProducts().forEach(System.out::println);
     }
 }
